7ee439ab3a31999be41ec4dbf9398a6970b87b9f
Maciej Kamiński <maciej.bruno-kaminski@comarch.pl>
2016-04-11 15:44:00 +0200

    Plugin includeSource zamiast wiredep

--- gruntfile.js
+++ gruntfile.js
@@ -1,35 +1,36 @@
-/**
- * Created by Maciej on 2016-04-10.
- */
+/** Created by Maciej on 2016-04-10. */
 module.exports = function(grunt) {
   grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
-      wiredep: {
-          task: {
-              src: ['index.html'],
-              options: {
-                  directory: '<%= bower.options.targetDir %>'
-              }
-          }
-      },
-      watch: {
-          files: ['<%= bower.options.targetDir %>'],
-          tasks: ['wiredep']
-      },
       bower: {
           install: {},
           options: {
-              "targetDir": "./assets/libs",
-              "layout": "byComponent",
-              "cleanup": true
+              targetDir: "assets/libs",
+              layout: "byComponent",
+              cleanBowerDir: true
+          }
+      },
+      includeSource: {
+          options: {},
+          myTarget: {
+            files: {"index.html" : "index.html"}
+          }
+      },
+      watch: {
+          libs: {
+              files: ['<%= bower.options.targetDir %>'],
+              tasks: ['includeSource'],
+              options: {
+                  event: ['added', 'deleted']
+              }
           }
       }
   });
 
-    grunt.loadNpmTasks('grunt-wiredep');
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-include-source');
 
-    grunt.registerTask('default', ['bower', 'wiredep']);
+    grunt.registerTask('default', ['bower', 'includeSource']);
     grunt.registerTask('changes', ['watch']);
 };
