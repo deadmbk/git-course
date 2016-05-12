59a789973284f8ca1663a347b974731916d663b1
Maciej Kamiński <maciej.bruno-kaminski@comarch.pl>
2016-05-12 16:14:34 +0200

    Dodano flow dla jednej lekcji (z dwoma etapami)

--- app/app.js
+++ app/app.js
@@ -1,58 +1,19 @@
-var myApp = angular.module('myApp', ['ng-terminal-example']);
+angular.module('myApp', ['ng-terminal-example', 'ngRoute'])
+    .config(['terminalConfigurationProvider', function(terminalConfigurationProvider) {
+        terminalConfigurationProvider.outputDelay = 0;
+        terminalConfigurationProvider.allowTypingWriteDisplaying = false;
+    }])
+    .config(['$routeProvider', function($routeProvider) {
 
-myApp.config(['terminalConfigurationProvider', function(terminalConfigurationProvider) {
-    terminalConfigurationProvider.outputDelay = 0;
-    terminalConfigurationProvider.allowTypingWriteDisplaying = false;
-}]);
+        $routeProvider.when('/', {
+            templateUrl: 'app/components/home/home.html'
+        }).when('/lesson/:id', {
+            templateUrl: 'app/components/lesson/lesson.html',
+            controller: 'lessonCtrl'
+        }).otherwise({redirectTo: '/'});
 
-myApp.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
+}]).controller('mainCtrl', ['$scope', function($scope) {
 
-        $scope.$broadcast('terminal-output', {
-            output: true,
-            text: ['Welcome to vtortola.GitHub.io',
-                'This is an example of ng-terminal-emulator.',
-                '',
-                "Please type 'help' to open a list of commands"],
-            breakLine: true
-        });
-
-    $scope.$on('terminal-input', function (e, consoleInput) {
-
-        /*
-
-        - Musi istnieć licznik dla stage'y
-        - lekcje w osobnych plikach - przy routerze będzie pobierany odpowiedni plik z jsonem, ładowany do obiektu js
-        - w oparciu o licznik potrzebna jet funkcja, która wydobędzie informacje o wybranym stage'u.
-
-         */
-
-        /*
-            1. Sprawdź czy polecenie jest poleceniem kończącym
-            2. Jeśli jest, wczytaj następnego stage'a
-            3. Jeśli nie, sprawdź czy komenda jest dozwolona - jest ok
-            4. Jeśli nie jest wyświetl komunikat z pola "error"
-
-         */
-
-
-        var cmd = consoleInput[0];
-       // console.log(cmd.command);
-
-        $http.get('app/cli.php?cmd=' + cmd.command)
-            .then(function(response) {
-
-                $scope.$broadcast('terminal-output', {
-                    output: true,
-                    text: [response.data.output],
-                    breakLine: true
-                });
-
-                console.log(response.data);
-            });
-        // do stuff
-
-
-    });
 
 
 }]);
