e26d1636f0e9f5f0b09692f4911ee8c6b20226e0
Maciej Kamiński <maciej.bruno-kaminski@comarch.pl>
2016-06-07 11:30:01 +0200

    Dodanie liczby ukończonych lekcji do widoku kończącego

--- app/components/finish/finishController.js
+++ app/components/finish/finishController.js
@@ -5,19 +5,45 @@
         .module('myApp')
         .controller('finishController', finishController);
 
-    finishController.$inject = ['GENERAL_CONFIG', '$location', 'storageService'];
+    finishController.$inject = ['GENERAL_CONFIG', '$location', 'storageService', 'lessonService'];
 
-    function finishController(CONFIG, $location, storageService) {
+    function finishController(CONFIG, $location, storageService, lessonService) {
 
         var vm = this;
 
+        vm.lessonCount;
+        vm.lessonDone;
         vm.start = startCourse;
 
+        init();
+
+        // -------------------------------------------------------------
+        function init() {
+
+            lessonService.getLessons()
+                .then(onReceivedLessons, onError);
+
+        }
+
+        // -------------------------------------------------------------
+        function onReceivedLessons(data) {
+            vm.lessonCount = data.length;
+            vm.lessonDone =  storageService.getFinishedLessons().length;
+        }
+
+        // -------------------------------------------------------------
+        function onError(reason) {
+            $log.error(reason);
+        }
+
+        // -------------------------------------------------------------
         function startCourse() {
             storageService.resetFinishedLessons();
             storageService.setCurrentLesson(1);
             return $location.path(CONFIG.LESSON_URI + '1');
         }
+
+
 
     }
 
